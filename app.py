a4fe2e535989573e897c8868f9f34fcdcb2c468e
jingwen <jingwenc@umich.edu>
2018-04-17 21:55:05 -0400

    finished part1 and part2, ec1 and ec2

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     ## print the guestbook
-    return render_template("index.html", entries=model.get_entries())
+    return render_template("admin.html", entries=model.get_entries())
 
 @app.route("/add")
 def addentry():
@@ -19,6 +19,19 @@ def postentry():
     message = request.form["message"]
     model.add_entry(name, message)
     return redirect("/")
+
+@app.route("/delete", methods=["POST"])
+def postdelete():
+    id = request.form['id']
+    model.delete_entry(int(id))
+    return redirect('/')
+
+@app.route("/modify", methods=["POST"])
+def postmodify():
+    id = request.form['id']
+    text=request.form['new_message']
+    model.modify_entry(int(id),text)
+    return redirect('/')
 
 if __name__=="__main__":
     model.init()
--- model.py
+++ model.py
@@ -5,10 +5,9 @@ from datetime import datetime
 GUESTBOOK_ENTRIES_FILE = "entries.json"
 entries = []
 
-def init(app):
+def init():
     global entries
     try:
-
         f = open(GUESTBOOK_ENTRIES_FILE)
         entries = json.loads(f.read())
         f.close()
@@ -23,8 +22,26 @@ def get_entries():
 def add_entry(name, text):
     global entries, GUESTBOOK_ENTRIES_FILE
     now = datetime.now()
-    time_string = now.strftime("%b %d, %Y %-I:%M %p")
-    entry = {"author": name, "text": text, "timestamp": time_string}
+    # time_string = now.strftime("%b %d, %Y %-I:%M %p")
+    time_string = str(now)
+    try:
+        with open(GUESTBOOK_ENTRIES_FILE,'r') as f:
+            entries=json.load(f)
+            max_id=-1
+            for i in entries:
+                if 'id' in i.keys():
+                    if int(i['id']) > max_id:
+                        max_id=int(i['id'])
+            for j in entries:
+                if len(entries) != 0:
+                    if 'id' not in j.keys():
+                        max_id+=1
+                        j['id']=max_id
+            next_id=max_id+1
+            print(next_id)
+    except:
+        print('Error!')
+    entry = {"author": name, "text": text, "timestamp": time_string,"id":str(next_id)}
     entries.insert(0, entry) ## add to front of list
     try:
         f = open(GUESTBOOK_ENTRIES_FILE, "w")
@@ -33,3 +50,49 @@ def add_entry(name, text):
         f.close()
     except:
         print("ERROR! Could not write entries to file.")
+
+
+def delete_entry(id):
+    global entries, GUESTBOOK_ENTRIES_FILE
+    try:
+        with open(GUESTBOOK_ENTRIES_FILE,'r+') as f:
+            entries=json.load(f)
+            for i in entries:
+                if id == int(i['id']):
+                    entries.remove(i)
+            f.seek(0)
+            f.truncate()
+            json.dump(entries, f)
+        f = open(GUESTBOOK_ENTRIES_FILE)
+        entries = json.loads(f.read())
+        f.close()
+        # with open(GUESTBOOK_ENTRIES_FILE) as f:
+        #     entries=json.load(f)
+        #     print(entries)
+        print('Deleted id:',id)
+    except:
+        print('Error!')
+
+def modify_entry(id,text):
+    global entries, GUESTBOOK_ENTRIES_FILE
+    now = datetime.now()
+    # time_string = now.strftime("%b %d, %Y %-I:%M %p")
+    # if you have an error using this format, just use
+    time_string = str(now)
+    try:
+        with open(GUESTBOOK_ENTRIES_FILE,'r+') as f:
+            content=json.load(f)
+            for i in content:
+                if id == int(i['id']):
+                    i['text']=text
+                    i['timestamp']=time_string
+            entries = content
+            f.seek(0)
+            f.truncate()
+            json.dump(content, f)
+        # with open(GUESTBOOK_ENTRIES_FILE) as f:
+        #     content=json.load(f)
+        #     print(content)
+        print('Modified id:',id)
+    except:
+        print('Error!')
